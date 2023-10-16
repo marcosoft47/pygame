dff2cc0b53b04a6908b9bdc1aec163c89b8e3d8d
marcosoft47 <56490702+marcosoft47@users.noreply.github.com>
2023-10-16 11:59:12 -0300

    coisei o negócio de spritesheet

--- testes/spritesheet/spr.py
+++ testes/spritesheet/spr.py
@@ -19,14 +19,17 @@ AZULCLARO = (192,255,255)
 BRANCO = (255,255,255)
 
 speed = 5
+bateu = False
 
 spr_kid = pygame.image.load(os.path.join(path_imagens, 'kid.png')).convert_alpha()
 spr_fruit = pygame.image.load(os.path.join(path_imagens, 'fruit.png')).convert_alpha()
 spr_spike = pygame.image.load(os.path.join(path_imagens, 'spike.png')).convert_alpha()
 spr_block = pygame.image.load(os.path.join(path_imagens, 'block.png')).convert_alpha()
 spr_cloud = pygame.image.load(os.path.join(path_imagens, 'cloud.png')).convert_alpha()
+spr_spike = pygame.image.load(os.path.join(path_imagens, 'spike.png')).convert_alpha()
 
 snd_pulo = pygame.mixer.Sound(os.path.join(path_sons, 'teleport_in.wav'))
+snd_gameover = pygame.mixer.Sound(os.path.join(path_sons, 'gameover.ogg'))
 pygame.mixer.music.load(os.path.join(path_sons, 'moon.ogg'))
 pygame.mixer.music.set_volume(.5)
 pygame.mixer.music.play(-1)
@@ -44,13 +47,14 @@ class Kid(pygame.sprite.Sprite):
         self.index_frames = 0
         self.image = self.frames_kid[self.index_frames]
         self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
         self.pos_y_inicial = altura - 64*3
         self.rect.center = (largura//3, self.pos_y_inicial)
         self.pulo = False
 
     def update(self):
         if self.pulo:
-            if self.rect.y <= altura//6:
+            if self.rect.y <= altura//4:
                 self.pulo = False
             self.rect.y -= speed*2
         else:
@@ -95,6 +99,18 @@ class Chao(pygame.sprite.Sprite):
             self.rect.x = largura
         self.rect.x -= 5
 
+class Espinho(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = spr_spike
+        self.image = pygame.transform.scale(self.image, (32*2, 32*2))
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.bottomleft = (largura, altura-64)
+    def update(self):
+        if self.rect.topright[0] < 0:
+            self.rect.x = largura
+        self.rect.x -= 5
 
 allsprites = pygame.sprite.Group()
 
@@ -106,7 +122,11 @@ for i in range(largura*10//64):
     chao = Chao(i)
     allsprites.add(chao)
 
+espinho = Espinho()
+allsprites.add(espinho)
 
+allspikes = pygame.sprite.Group()
+allspikes.add(espinho)
 
 kid = Kid()
 allsprites.add(kid)
@@ -125,7 +145,16 @@ while True:
                 if kid.rect.y == kid.pos_y_inicial:
                     kid.pular()
     
+    colisoes = pygame.sprite.spritecollide(kid, allspikes, False, pygame.sprite.collide_mask)
+
     allsprites.draw(tela)
-    allsprites.update()
+    if colisoes and not bateu:
+        bateu = True
+        pygame.mixer.music.stop()
+        snd_gameover.play()
+    if bateu:
+        pass
+    else:
+        allsprites.update()
 
     pygame.display.flip()
