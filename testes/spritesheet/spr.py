048c9c03347f7f70519dbd712bb8bc3b41c6f584
marcosoft47 <56490702+marcosoft47@users.noreply.github.com>
2023-10-16 14:08:01 -0300

    adicionado a fruta deliciosa

--- testes/spritesheet/spr.py
+++ testes/spritesheet/spr.py
@@ -14,19 +14,19 @@ path_sons = os.path.join(pwd, 'sons')
 largura = 1280
 altura = 640
 tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('Guy ')
+pygame.display.set_caption('Guy')
 AZULCLARO = (192,255,255)
 BRANCO = (255,255,255)
 
 speed = 5
 bateu = False
+escolha_obstaculo = random.choice([0,1])
 
 spr_kid = pygame.image.load(os.path.join(path_imagens, 'kid.png')).convert_alpha()
 spr_fruit = pygame.image.load(os.path.join(path_imagens, 'fruit.png')).convert_alpha()
 spr_spike = pygame.image.load(os.path.join(path_imagens, 'spike.png')).convert_alpha()
 spr_block = pygame.image.load(os.path.join(path_imagens, 'block.png')).convert_alpha()
 spr_cloud = pygame.image.load(os.path.join(path_imagens, 'cloud.png')).convert_alpha()
-spr_spike = pygame.image.load(os.path.join(path_imagens, 'spike.png')).convert_alpha()
 
 snd_pulo = pygame.mixer.Sound(os.path.join(path_sons, 'teleport_in.wav'))
 snd_gameover = pygame.mixer.Sound(os.path.join(path_sons, 'gameover.ogg'))
@@ -107,12 +107,35 @@ class Espinho(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
         self.rect.bottomleft = (largura, altura-64)
+        self.rect.x = largura
+
+        self.escolha = escolha_obstaculo
     def update(self):
-        if self.rect.topright[0] < 0:
-            self.rect.x = largura
-        self.rect.x -= 5
+        if self.escolha == 0:
+            if self.rect.topright[0] < 0:
+                self.rect.x = largura
+            self.rect.x -= 5
+
+class Fruta(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = spr_fruit
+        self.image = pygame.transform.scale(self.image, (32*2,32*2))
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.center = (largura, 300)
+        self.rect.x = largura
+
+        self.escolha = escolha_obstaculo
+    def update(self):
+        if self.escolha == 1:
+            if self.rect.topright[0] < 0:
+                self.rect.x = largura
+            self.rect.x -= 5
+
 
 allsprites = pygame.sprite.Group()
+obstaculos = pygame.sprite.Group()
 
 for i in range(20):
     nuvem = Nuvens()
@@ -125,8 +148,11 @@ for i in range(largura*10//64):
 espinho = Espinho()
 allsprites.add(espinho)
 
-allspikes = pygame.sprite.Group()
-allspikes.add(espinho)
+obstaculos.add(espinho)
+
+fruta = Fruta()
+allsprites.add(fruta)
+obstaculos.add(fruta)
 
 kid = Kid()
 allsprites.add(kid)
@@ -145,7 +171,14 @@ while True:
                 if kid.rect.y == kid.pos_y_inicial:
                     kid.pular()
     
-    colisoes = pygame.sprite.spritecollide(kid, allspikes, False, pygame.sprite.collide_mask)
+    if espinho.rect.topright[0] <= 0 or fruta.rect.topright[0] <= 0:
+        escolha_obstaculo = random.choice([0,1])
+        espinho.rect.x = largura
+        fruta.rect.x = largura
+        espinho.escolha = escolha_obstaculo
+        fruta.escolha = escolha_obstaculo
+
+    colisoes = pygame.sprite.spritecollide(kid, obstaculos, False, pygame.sprite.collide_mask)
 
     allsprites.draw(tela)
     if colisoes and not bateu:
